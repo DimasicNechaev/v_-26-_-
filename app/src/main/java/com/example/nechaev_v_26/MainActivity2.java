159cbea0d745ad408bd0d4be023560fd3c342246
DimasicNechaev <diman00nechaev@gmail.com>
2021-12-25 15:45:01 +0700

    Nechaev

---
+++ app/src/main/java/com/example/nechaev_v_26/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.nechaev_v_26;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
